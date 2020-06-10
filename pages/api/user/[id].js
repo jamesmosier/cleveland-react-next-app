4519d9a99ff9fcecc07f07002dedcb4908b09ba2
James Mosier <2854919+jamesmosier@users.noreply.github.com>
2020-06-09 23:32:21 -0400

    move getUserById into separate file

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,3 +1,5 @@
+import getUserById from '../../../lib/getUserById';
+
 export default async function UserApi(req, res) {
   const {
     query: { id },
@@ -8,16 +10,4 @@ export default async function UserApi(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(user));
-}
-
-function getUserById(id) {
-  return new Promise(function (resolve) {
-    const user = {
-      id,
-      name: 'Billy Blanks',
-      username: 'bblanks',
-      email: 'billy.blanks@msn.com',
-    };
-    setTimeout(resolve.bind(null, user), 1000);
-  });
 }
